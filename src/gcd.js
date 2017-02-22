e98bdc1f206e457281813dbf1d64a0ae062f3be1
Igor Kusoff <igor.kusoff@gmail.com>
2017-02-22 18:07:53 +0000

    make gcd game

---
+++ src/gcd.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+import askName from './';
+import randomNumber from './random-number';
+import getGcd from './get-gcd';
+
+export default () => {
+  const name = askName();
+  const iter = (count) => {
+    if (count > 2) {
+      return console.log(`Congratulations, ${name}!`);
+    }
+    const num1 = randomNumber();
+    const num2 = randomNumber();
+    const result = getGcd(num1, num2);
+    console.log(`Question: ${num1} ${num2}`);
+    const answer = readlineSync.question('Your answer: ');
+  if (result !== Number(answer)) {
+  return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result}'.\nLet's try again, ${name}!`);
+  }
+    console.log('Correct!');
+    return iter(count + 1);
+  };
+  return iter(0);
+};
+
