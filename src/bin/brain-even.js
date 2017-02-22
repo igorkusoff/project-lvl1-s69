f835679a420aab699df10e4fa6f15e8d12b4c32a
Igor Kusoff <igor.kusoff@gmail.com>
2017-02-22 12:00:32 +0000

    remove import askName from index

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import start from './../even';
 
-console.log('Welcome to the Brain Games!\n Answer "yes" if number even otherwise answer "no".\n');
+console.log('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".\n');
 start();
