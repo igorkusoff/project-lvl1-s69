6c266950657d7bad7c22cc9c563926c633a7ca7a
Igor Kusoff <igor.kusoff@gmail.com>
2017-02-23 13:05:35 +0000

    remove extra func, update paths

--- src/bin/brain-calc.js
+++ src/bin/brain-calc.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import start from './../calc';
+import start from './../games/calc';
 
 start();
--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import start from './../even';
+import start from './../games/even';
 
 start();
