f7dee7409ecd1b20a506eef82c4c0c70c9b1a523
Igor Kusoff <igor.kusoff@gmail.com>
2017-02-23 12:00:30 +0000

    fix the output

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
-  console.log('\nWelcome to the Brain Games!\n');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  return name;
+  return console.log('Welcome to the Brain Games!\n');
+//  const name = readlineSync.question('May I have your name? ');
+//  console.log(`Hello, ${name}!`);
+//  return name;
 };
