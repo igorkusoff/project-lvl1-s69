731620554968475953f931899d8e61adcd2f10f3
Igor Kusoff <igor.kusoff@gmail.com>
2017-02-23 11:09:03 +0000

    add welcome

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
+  console.log('\nWelcome to the Brain Games!\n');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
