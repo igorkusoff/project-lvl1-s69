fac5f20b8a294d2d10a25491b32d7e4b020fddd6
Igor Kusoff <igor.kusoff@gmail.com>
2017-02-26 08:34:07 +0000

    rename function

--- src/games/even.js
+++ src/games/even.js
@@ -2,14 +2,14 @@ import flow from '../';
 
 const challenge = 'Answer "yes" if number even otherwise answer "no".';
 const count = 3;
-const isEven = (n) => {
+const correctAnswer = (n) => {
   const answer = n % 2 === 0 ? 'yes' : 'no';
   return answer;
 };
 const gameFlow = () => {
   const game = () => {
     const task = Math.floor(100 * Math.random());
-    const result = isEven(task);
+    const result = correctAnswer(task);
     return [task, result];
   };
   flow(challenge, game, count);
